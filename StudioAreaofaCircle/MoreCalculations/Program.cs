b81cbfc5b7bab8e657f777b6eba044cf0a6229d4
Melinda <codegirl78@gmail.com>
2021-07-14 19:54:51 -0500

    Added code to complete more calculations

--- StudioAreaofaCircle/MoreCalculations/Program.cs
+++ StudioAreaofaCircle/MoreCalculations/Program.cs
@@ -6,7 +6,24 @@ namespace MoreCalculations
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string input;
+            double pi;
+            double r;
+            double area;
+            double circumference;
+            double diameter;
+
+            Console.WriteLine("Please enter the radius of the circle: ");
+            input = Console.ReadLine();
+            pi = 3.14;
+            r = double.Parse(input);
+            area = (pi * r * r);
+            circumference = (2 * pi * r);
+            diameter = (2 * r);
+
+            Console.WriteLine("The area of a circle with a radius of " + r + " is: " + area);
+            Console.WriteLine("The circumference of a circle with a radius of " + r + " is: " + circumference);
+            Console.WriteLine("The diameter of a circle with a radius of " + r + " is: " + diameter);
         }
     }
 }
